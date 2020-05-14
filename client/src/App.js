58f680e21cd91b80dfff17238401dd1010d311f9
vishalicious213 <vishalicious213@gmail.com>
2020-05-14 15:32:58 -0400

    initial commit. added route to update movies.

--- client/src/App.js
+++ client/src/App.js
@@ -35,6 +35,10 @@ const App = () => {
       <Route path="/movies/:id">
         <Movie addToSavedList={addToSavedList} />
       </Route>
+
+      <Route path="/update-movie/:id">
+        {/* component to update form goes here */}
+      </Route>
     </>
   );
 };
