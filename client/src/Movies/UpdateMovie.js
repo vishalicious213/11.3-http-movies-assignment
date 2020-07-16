3ee034b43428de3124277ff5cc130134224df9ae
Vish Singh <vishalicious213@gmail.com>
2020-07-16 01:05:26 -0700

    UpdateMovie shows correct movie data. need to create form.

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useParams } from 'react-router-dom';
 
 const UpdateMovie = () => {
+    const [movie, setMovie] = useState(null);
     const params = useParams();
+
+    const fetchMovie = (id) => {
+        axios
+        .get(`http://localhost:5000/api/movies/${id}`)
+        .then((res) => setMovie(res.data))
+        .catch((err) => console.log(err.response));
+    };
+
+    useEffect(() => {
+        fetchMovie(params.id);
+    }, [params.id]);
+    
+    if (!movie) {
+        return <div>Loading movie information...</div>;
+    }
 
     console.log(params.id);
 
     return (
-        <div>{`-Update Movie ${params.id}-`}</div>
+        <section>
+            <div>{`-Update Movie ${params.id}-`}</div>
+            <div>{movie.title}</div>
+            <div>{movie.director}</div>
+            <div>{movie.metascore}</div>
+
+            {movie.stars.map(star => (
+                <div key={star} className="movie-star">{star}</div>
+            ))}
+        </section>
     )
 }
 
